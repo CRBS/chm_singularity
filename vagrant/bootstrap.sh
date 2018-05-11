2b075fd7319d1a10e1e5cbb9fc13cf8af619ba80
Chris Churas <churas.camera@gmail.com>
2018-05-11 10:43:01 -0700

    added yum update and a few other packages, dont need cmake but will leave for now

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 echo "Installing base packages"
-# yum install -y cmake epel-release
-# yum install -y opencv opencv-devel mpich mpich-devel mpich-autoload qt qt-devel wget tcsh xauth xclock gcc-c++ mlocate time tree xorg-x11-fonts-* mesa-* python-pip python-wheel
+yum -y update
+yum install -y cmake epel-release git
 
 yum install -y make gcc g++ wget gzip
 echo "Installing singularity"
